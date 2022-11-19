8ce7249df89cc32271d5827f02920d233c8e6994
Rajendrankr25 <rajendrankr25@gmail.com>
2022-11-19 10:07:20 +0530

    day2 task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { Free } from './Free';
+import { Plus } from './Plus';
+import { Pro } from './Pro';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Free />
+      <Plus />
+      <Pro />
     </div>
   );
 }
